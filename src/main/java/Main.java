d6a2a28f44a0ce40b5dbf79630624a1865192751
Adi <adisorin.olteanu@yahoo.com>
2021-12-22 18:44:38 +0200

    Changed name and icon picture.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 // OKHttp sau JKSON for APIS
 
@@ -13,9 +14,10 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
         Scene scene = new Scene(root, 1200, 800);
 
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Life Tracky");
         primaryStage.setScene(scene);
         primaryStage.show();
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("Photos/icon.png")));
     }
 
     public static void main(String[] args) {
